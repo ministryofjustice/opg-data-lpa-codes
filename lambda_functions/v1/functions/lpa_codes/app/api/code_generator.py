05892c33d3794c5fa27fb20d4dbcfe1b79583daf
Jennifer Mackown <jennifer.mackown@digital.justice.gov.uk>
2020-05-06 13:25:14 +0100

    Auto stash before rebase of "master"
    Get code record/s

--- lambda_functions/v1/functions/lpa_codes/app/api/code_generator.py
+++ lambda_functions/v1/functions/lpa_codes/app/api/code_generator.py
@@ -1,50 +1,38 @@
 import datetime
 
 import boto3
-from boto3.dynamodb.conditions import Key
+from boto3.dynamodb.conditions import Key, Attr
 
 
-def update_codes_by_key(key, status=False):
-
+def get_codes(key=None, code=None):
     table = boto3.resource("dynamodb").Table("lpa_codes")
+    return_fields = "lpa, actor, code"
 
-    set_active_flag = f"active = {status}"
-    set_status_updates = (
-        f"last_updated_date = " f"{datetime.datetime.now().strftime('%d/%m/%Y')}"
-    )
+    codes = []
 
-    all_codes = table.query(
-        KeyConditionExpression=Key("lpa").eq(key["lpa"]) & Key("actor").eq(key["actor"])
-    )
-
-    print(f"all_codes: {all_codes}")
-    for code in all_codes:
-        table.update_item(
-            Key={"lpa": key["lpa"], "actor": key["actor"]},
-            UpdateExpression=f"SET {set_active_flag}, {set_status_updates}",
+    if code:
+        query_result = table.get_item(
+            Key={"code": code}, ProjectionExpression=return_fields
         )
 
-    return "updated"
+        try:
+            codes.append(query_result["Item"])
+        except KeyError:
+            print("code does not exist")
 
+    elif key:
+        lpa = key["lpa"]
+        actor = key["actor"]
+        query_result = table.query(
+            IndexName="key_index",
+            KeyConditionExpression=Key("lpa").eq(lpa),
+            FilterExpression=Attr("actor").eq(actor),
+            ProjectionExpression=return_fields,
+        )
 
-def update_codes_by_code(code, status=False):
-    table = boto3.resource("dynamodb").Table("lpa_codes")
+        if len(query_result["Items"]) > 0:
+            codes.extend(query_result["Items"])
+        else:
+            print("key does not exist")
 
-    set_active_flag = f"active = {status}"
-    set_status_updates = (
-        f"last_updated_date = " f"{datetime.datetime.now().strftime('%d/%m/%Y')}"
-    )
-
-    try:
-        key = table.query(
-            IndexName="code_index", KeyConditionExpression=Key("code").eq(code)
-        )["Items"][0]
-    except IndexError:
-        return "code does not exist"
-
-    table.update_item(
-        Key={"lpa": key["lpa"], "actor": key["actor"], "code": key["code"]},
-        UpdateExpression=f"SET {set_active_flag}, {set_status_updates}",
-    )
-
-    return "updated"
+    return codes
--- lambda_functions/v1/tests/code_generator/conftest.py
+++ lambda_functions/v1/tests/code_generator/conftest.py
@@ -17,7 +17,7 @@ def mock_unique_code(monkeypatch):
     monkeypatch.setattr(code_generator, "check_code_unique", unique_true)
 
 
-@pytest.fixture(scope="session", autouse=False)
+@pytest.fixture(scope="function", autouse=False)
 def mock_database():
     with mock_dynamodb2():
         print("db setup")
@@ -26,23 +26,26 @@ def mock_database():
         table = mock_db.create_table(
             TableName="lpa_codes",
             KeySchema=[
-                {"AttributeName": "lpa", "KeyType": "HASH"},  # Partition key
-                {"AttributeName": "actor", "KeyType": "RANGE"},  # Sort key
-                # {"AttributeName": "code", "KeyType": "RANGE"},  # Sort key
+                {"AttributeName": "code", "KeyType": "HASH"},  # Partition key
+                # {"AttributeName": "lpa", "KeyType": "RANGE"},  # Sort key
+                # {"AttributeName": "actor", "KeyType": "RANGE"},  # Sort key
             ],
             AttributeDefinitions=[
-                {"AttributeName": "lpa", "AttributeType": "S"},
-                {"AttributeName": "actor", "AttributeType": "S"},
-                # {"AttributeName": "code", "AttributeType": "S"},
+                {"AttributeName": "code", "AttributeType": "S"},
+                # {"AttributeName": "lpa", "AttributeType": "S"},
+                # {"AttributeName": "actor", "AttributeType": "S"},
             ],
             ProvisionedThroughput={"ReadCapacityUnits": 10, "WriteCapacityUnits": 10},
         )
 
-        code_index = [
+        key_index = [
             {
                 "Create": {
-                    "IndexName": "code_index",
-                    "KeySchema": [{"AttributeName": "code", "KeyType": "HASH"}],
+                    "IndexName": "key_index",
+                    "KeySchema": [
+                        {"AttributeName": "lpa", "KeyType": "HASH"},
+                        {"AttributeName": "actor", "KeyType": "RANGE"},
+                    ],
                     "Projection": {"ProjectionType": "ALL"},
                     "ProvisionedThroughput": {
                         "ReadCapacityUnits": 5,
@@ -66,14 +69,82 @@ def mock_database():
             },
         ]
 
-        table.update(GlobalSecondaryIndexUpdates=code_index)
+        table.update(GlobalSecondaryIndexUpdates=key_index)
         table.update(GlobalSecondaryIndexUpdates=active_index)
 
-        # data = test_data()
-        # for row in data:
-        #     table.put_item(Item=row)
+        data = test_data()
+        for row in data:
+            table.put_item(Item=row)
 
         yield mock_db
 
         print("db teardown")
         table.delete()
+
+
+def test_data():
+    return [
+        {
+            "active": True,
+            "actor": "mediumblue",
+            "code": "YsSu4iAztUXm",
+            "expiry_date": "29/03/2021",
+            "generated_date": "29/03/2020",
+            "last_updated_date": "25/08/2020",
+            "lpa": "drive_leading-edge_communities",
+        },
+        {
+            "active": True,
+            "actor": "mediumblue",
+            "code": "aEYVS6i9zSwy",
+            "expiry_date": "26/06/2020",
+            "generated_date": "27/06/2019",
+            "last_updated_date": "03/02/2020",
+            "lpa": "drive_leading-edge_communities",
+        },
+        {
+            "active": True,
+            "actor": "mediumblue",
+            "code": "ZY577rXcRVLY",
+            "expiry_date": "05/04/2021",
+            "generated_date": "05/04/2020",
+            "last_updated_date": "28/02/2021",
+            "lpa": "drive_leading-edge_communities",
+        },
+        {
+            "active": False,
+            "actor": "mediumblue",
+            "code": "hFCarGyJF6G2",
+            "expiry_date": "06/08/2020",
+            "generated_date": "07/08/2019",
+            "last_updated_date": "24/03/2020",
+            "lpa": "drive_leading-edge_communities",
+        },
+        {
+            "active": False,
+            "actor": "mediumblue",
+            "code": "hm8Qtyb763tD",
+            "expiry_date": "05/08/2020",
+            "generated_date": "06/08/2019",
+            "last_updated_date": "01/12/2019",
+            "lpa": "drive_leading-edge_communities",
+        },
+        {
+            "active": False,
+            "actor": "mediumblue",
+            "code": "HiRqUNXRKB3X",
+            "expiry_date": "02/01/2021",
+            "generated_date": "03/01/2020",
+            "last_updated_date": "11/04/2020",
+            "lpa": "drive_leading-edge_communities",
+        },
+        {
+            "active": False,
+            "actor": "mediumblue",
+            "code": "UEW7zSi42bLF",
+            "expiry_date": "09/03/2021",
+            "generated_date": "09/03/2020",
+            "last_updated_date": "17/10/2020",
+            "lpa": "drive_leading-edge_communities",
+        },
+    ]
