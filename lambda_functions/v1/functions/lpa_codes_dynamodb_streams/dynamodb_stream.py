716f0bdaa47fd495a5500fd382a7543498905f63
james.warren <james.warren@digital.justice.gov.uk>
2020-06-02 09:55:12 +0100

    IN-202 redact codes from streams

--- lambda_functions/v1/functions/lpa_codes_dynamodb_streams/dynamodb_stream.py
+++ lambda_functions/v1/functions/lpa_codes_dynamodb_streams/dynamodb_stream.py
@@ -3,5 +3,17 @@ from __future__ import print_function
 
 def lambda_handler(event, context):
     for record in event["Records"]:
-        print(record["eventName"])
-        print(record["dynamodb"])
+        record_to_print = {}
+        record["dynamodb"]["NewImage"]["code"] = hash(
+            str(record["dynamodb"]["NewImage"]["code"])
+        )
+        record_to_print["EventName"] = record["eventName"]
+        record_to_print["NewImage"] = record["dynamodb"]["NewImage"]
+
+        if "OldImage" in record["dynamodb"]:
+            record["dynamodb"]["OldImage"]["code"] = hash(
+                str(record["dynamodb"]["OldImage"]["code"])
+            )
+            record_to_print["OldImage"] = record["dynamodb"]["OldImage"]
+
+        print(str(record_to_print))
