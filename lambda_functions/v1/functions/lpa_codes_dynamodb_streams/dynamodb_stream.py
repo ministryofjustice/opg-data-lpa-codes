6c1011bca6e966e968d21492e22ddbf8e4e0fec6
james.warren <james.warren@digital.justice.gov.uk>
2020-06-02 10:22:38 +0100

    lets make it json

--- lambda_functions/v1/functions/lpa_codes_dynamodb_streams/dynamodb_stream.py
+++ lambda_functions/v1/functions/lpa_codes_dynamodb_streams/dynamodb_stream.py
@@ -1,16 +1,19 @@
+import json
+
+
 def lambda_handler(event, context):
     for record in event["Records"]:
-        record_to_print = {}
+        recordtoprint = {}
         record["dynamodb"]["NewImage"]["code"] = hash(
             str(record["dynamodb"]["NewImage"]["code"])
         )
-        record_to_print["EventName"] = record["eventName"]
-        record_to_print["NewImage"] = record["dynamodb"]["NewImage"]
+        recordtoprint["EventName"] = record["eventName"]
+        recordtoprint["NewImage"] = record["dynamodb"]["NewImage"]
 
         if "OldImage" in record["dynamodb"]:
             record["dynamodb"]["OldImage"]["code"] = hash(
                 str(record["dynamodb"]["OldImage"]["code"])
             )
-            record_to_print["OldImage"] = record["dynamodb"]["OldImage"]
+            recordtoprint["OldImage"] = record["dynamodb"]["OldImage"]
 
-        print(str(record_to_print))
+        print(json.dumps(recordtoprint))
