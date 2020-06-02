571ed8b582ed3474fd8c9ccda01db014fe5a49f7
james.warren <james.warren@digital.justice.gov.uk>
2020-06-02 16:24:48 +0100

    make circle better.. probably

--- .circleci/set_env.sh
+++ .circleci/set_env.sh
@@ -8,24 +8,27 @@ SIRIUS_GITHUB_URL="api.github.com/repos/ministryofjustice/opg-sirius"
 ACCOUNT="997462338508"
 ENVIRONMENT="ci"
 
-export SECRET_STRING=$(aws sts assume-role \
---role-arn "arn:aws:iam::${ACCOUNT}:role/get-pact-secret-production" \
---role-session-name AWSCLI-Session | \
-jq -r '.Credentials.SessionToken + " " + .Credentials.SecretAccessKey + " " + .Credentials.AccessKeyId')
+if [ $(aws --version 2>&1 | grep -c "aws-cli") -ne 0 ]
+then
+  export SECRET_STRING=$(aws sts assume-role \
+  --role-arn "arn:aws:iam::${ACCOUNT}:role/get-pact-secret-production" \
+  --role-session-name AWSCLI-Session | \
+  jq -r '.Credentials.SessionToken + " " + .Credentials.SecretAccessKey + " " + .Credentials.AccessKeyId')
 
-#local export so they only exist in this stage
-export AWS_ACCESS_KEY_ID=$(echo "${SECRET_STRING}" | awk -F' ' '{print $3}')
-export AWS_SECRET_ACCESS_KEY=$(echo "${SECRET_STRING}" | awk -F' ' '{print $2}')
-export AWS_SESSION_TOKEN=$(echo "${SECRET_STRING}" | awk -F' ' '{print $1}')
+  #local export so they only exist in this stage
+  export AWS_ACCESS_KEY_ID=$(echo "${SECRET_STRING}" | awk -F' ' '{print $3}')
+  export AWS_SECRET_ACCESS_KEY=$(echo "${SECRET_STRING}" | awk -F' ' '{print $2}')
+  export AWS_SESSION_TOKEN=$(echo "${SECRET_STRING}" | awk -F' ' '{print $1}')
 
-export PACT_BROKER_PASSWORD=$(aws secretsmanager get-secret-value \
---secret-id pactbroker_admin \
---region eu-west-1 | jq -r '.SecretString')
+  export PACT_BROKER_PASSWORD=$(aws secretsmanager get-secret-value \
+  --secret-id pactbroker_admin \
+  --region eu-west-1 | jq -r '.SecretString')
 
-export GITHUB_STATUS_CREDENTIALS=$(aws secretsmanager get-secret-value \
---secret-id integrations_github_credentials \
---region eu-west-1 | jq -r '.SecretString')
+  export GITHUB_STATUS_CREDENTIALS=$(aws secretsmanager get-secret-value \
+  --secret-id integrations_github_credentials \
+  --region eu-west-1 | jq -r '.SecretString')
 
+fi
 WORKSPACE=${WORKSPACE:-$CIRCLE_BRANCH}
 WORKSPACE=${WORKSPACE//[^[:alnum:]]/}
 WORKSPACE=${WORKSPACE,,}
