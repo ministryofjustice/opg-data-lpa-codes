1f676f414bc10097fa4aa29defe7fa0deb63d40d
Jennifer Mackown <jennifer.mackown@digital.justice.gov.uk>
2020-06-05 08:34:13 +0100

    Add missing unit test scenarios - handle_revoke

--- lambda_functions/v1/tests/api/cases_handle_revoke.py
+++ lambda_functions/v1/tests/api/cases_handle_revoke.py
@@ -80,3 +80,28 @@ def case_revoke_a_code_3() -> CaseData:
     expected_result = {"codes revoked": 0}
     expected_last_updated_date = "2019-12-26"
     return test_data, data, expected_result, expected_last_updated_date
+
+
+@case_name("Revoke a code by sending empty string")
+def case_revoke_a_code_in233() -> CaseData:
+    test_data = [
+        {
+            "active": True,
+            "actor": "violet",
+            "code": "jmABs6fFaNJG",
+            "expiry_date": test_constants["EXPIRY_DATE"],
+            "generated_date": "2019-09-31",
+            "last_updated_date": "2019-12-26",
+            "dob": "1960-06-05",
+            "lpa": "mesh_end_to_end_systems",
+            "status_details": "Generated",
+        }
+    ]
+
+    data = {
+        "code": "",
+    }
+
+    expected_result = {"codes revoked": 0}
+    expected_last_updated_date = test_constants["TODAY_ISO"]
+    return test_data, data, expected_result, expected_last_updated_date
