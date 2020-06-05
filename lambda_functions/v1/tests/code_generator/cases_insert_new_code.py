74493a4cbc84f8571825463c7b8f7c2749dd0865
Jennifer Mackown <jennifer.mackown@digital.justice.gov.uk>
2020-06-05 16:41:31 +0100

    insert new code improvement

--- lambda_functions/v1/tests/code_generator/cases_insert_new_code.py
+++ lambda_functions/v1/tests/code_generator/cases_insert_new_code.py
@@ -25,4 +25,16 @@ def case_insert_a_code() -> CaseData:
         }
     ]
 
-    return key, code, dob, expected_result
+    expected_row = {
+        "lpa": "f2dcb20f-b5d7-4c5e-8164-0408ea033a31",
+        "actor": "6f41cde2-f5f2-45d9-8776-e6dcdb1b56e8",
+        "code": "2gVYdRNjUHTX",
+        "active": True,
+        "last_updated_date": test_constants["TODAY_ISO"],
+        "dob": "1960-06-05",
+        "expiry_date": test_constants["EXPIRY_DATE"],
+        "generated_date": test_constants["TODAY_ISO"],
+        "status_details": "Generated",
+    }
+
+    return key, code, dob, expected_result, expected_row
