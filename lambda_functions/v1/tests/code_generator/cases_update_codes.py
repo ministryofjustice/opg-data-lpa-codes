a3c6696389c186a83af23095e0acb56344af02ef
Jennifer Mackown <jennifer.mackown@digital.justice.gov.uk>
2020-06-05 08:52:54 +0100

    Add missing unit test scenarios - update codes Plus bonus scenario for handle_create Missed one

--- lambda_functions/v1/tests/code_generator/cases_update_codes.py
+++ lambda_functions/v1/tests/code_generator/cases_update_codes.py
@@ -1,4 +1,6 @@
-from pytest_cases import CaseData, case_name, case_tags
+import copy
+
+from pytest_cases import CaseData, case_name, case_tags, cases_generator
 from lambda_functions.v1.tests.conftest import test_constants
 
 
@@ -188,6 +190,32 @@ def case_update_single_active_codes_to_active() -> CaseData:
     return test_data, key, code, status, expected_result
 
 
+@case_name("Set single existing code to inactive, but it's already inactive")
+def case_update_single_active_codes_to_active_in233() -> CaseData:
+
+    test_data = [
+        {
+            "active": False,
+            "actor": "palevioletred",
+            "code": "YYK8tX6wKk6K",
+            "expiry_date": test_constants["EXPIRY_DATE"],
+            "generated_date": "03/04/2020",
+            "last_updated_date": "01/03/2021",
+            "dob": "1960-06-05",
+            "lpa": "revolutionize_out_of_the_box_paradigms",
+        }
+    ]
+
+    key = None
+    code = "YYK8tX6wKk6K"
+
+    status = False
+
+    expected_result = 0
+
+    return test_data, key, code, status, expected_result
+
+
 @case_name("Try to update entries but key doesn't exist")
 def case_update_nonexistant_codes_to_active() -> CaseData:
 
@@ -206,6 +234,64 @@ def case_update_nonexistant_codes_to_active() -> CaseData:
 
     key = {
         "lpa": "this_key_does_not_exist",
+        "actor": "neither_doess_this_actor",
+    }
+    code = None
+
+    status = False
+
+    expected_result = 0
+
+    return test_data, key, code, status, expected_result
+
+
+@case_name("Try to update entries but lpa doesn't exist")
+def case_update_nonexistant_codes_to_active_in233_1() -> CaseData:
+
+    test_data = [
+        {
+            "active": True,
+            "actor": "royalblue",
+            "code": "3XgMCwsiF2h2",
+            "expiry_date": test_constants["EXPIRY_DATE"],
+            "generated_date": "16/05/2019",
+            "last_updated_date": "06/04/2020",
+            "dob": "1960-06-05",
+            "lpa": "embrace_rich_initiatives",
+        }
+    ]
+
+    key = {
+        "lpa": "this_key_does_not_exist",
+        "actor": "royalblue",
+    }
+    code = None
+
+    status = False
+
+    expected_result = 0
+
+    return test_data, key, code, status, expected_result
+
+
+@case_name("Try to update entries but actor doesn't exist")
+def case_update_nonexistant_codes_to_active_in233_2() -> CaseData:
+
+    test_data = [
+        {
+            "active": True,
+            "actor": "royalblue",
+            "code": "3XgMCwsiF2h2",
+            "expiry_date": test_constants["EXPIRY_DATE"],
+            "generated_date": "16/05/2019",
+            "last_updated_date": "06/04/2020",
+            "dob": "1960-06-05",
+            "lpa": "embrace_rich_initiatives",
+        }
+    ]
+
+    key = {
+        "lpa": "embrace_rich_initiatives",
         "actor": "neither_doess_this_actor",
     }
     code = None
@@ -243,29 +329,71 @@ def case_update_single_codes_1() -> CaseData:
     return test_data, key, code, status, expected_result
 
 
-@case_name("Set multiple existing codes to active")
-def case_update_multiple_codes_1() -> CaseData:
+@cases_generator(
+    "Try and update Entries when actor and LPA is blank - {item}", item=["actor", "lpa"]
+)
+def case_update_multiple_codes_to_inactive_in233(item) -> CaseData:
 
     test_data = [
         {
             "active": True,
-            "actor": "royalblue",
-            "code": "3XgMCwsiF2h2",
+            "actor": "salmon",
+            "code": "yEFJH33hn5nX",
             "expiry_date": test_constants["EXPIRY_DATE"],
-            "generated_date": "16/05/2019",
-            "last_updated_date": "06/04/2020",
+            "generated_date": "11/11/2019",
+            "last_updated_date": "19/06/2020",
             "dob": "1960-06-05",
-            "lpa": "embrace_rich_initiatives",
+            "lpa": "streamline_distributed_content",
         }
     ]
 
-    key = {
-        "lpa": "embrace_rich_initiatives",
-        "actor": "royalblue",
+    default_key = {
+        "lpa": "streamline_distributed_content",
+        "actor": "salmon",
     }
+
+    key = copy.deepcopy(default_key)
+    key[item] = ""
+
     code = None
 
-    status = True
+    status = False
+
+    expected_result = 0
+
+    return test_data, key, code, status, expected_result
+
+
+@cases_generator(
+    "Try and update Entries when actor and LPA is missing - {item}",
+    item=["actor", "lpa"],
+)
+def case_update_multiple_codes_to_inactive_in233_1(item) -> CaseData:
+
+    test_data = [
+        {
+            "active": True,
+            "actor": "salmon",
+            "code": "yEFJH33hn5nX",
+            "expiry_date": test_constants["EXPIRY_DATE"],
+            "generated_date": "11/11/2019",
+            "last_updated_date": "19/06/2020",
+            "dob": "1960-06-05",
+            "lpa": "streamline_distributed_content",
+        }
+    ]
+
+    default_key = {
+        "lpa": "streamline_distributed_content",
+        "actor": "salmon",
+    }
+
+    key = copy.deepcopy(default_key)
+    key.pop(item)
+
+    code = None
+
+    status = False
 
     expected_result = 0
 
