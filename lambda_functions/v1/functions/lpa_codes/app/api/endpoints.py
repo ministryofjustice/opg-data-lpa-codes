738f9832f8b95206b83db7f1ebe0f3b411a7dac7
Jennifer Mackown <jennifer.mackown@digital.justice.gov.uk>
2020-06-05 08:34:05 +0100

    Add missing unit test scenarios - handle_create

--- lambda_functions/v1/functions/lpa_codes/app/api/endpoints.py
+++ lambda_functions/v1/functions/lpa_codes/app/api/endpoints.py
@@ -19,31 +19,37 @@ def handle_create(data):
 
     for entry in data["lpas"]:
 
-        key = {"lpa": entry["lpa"], "actor": entry["actor"]}
-        dob = entry["dob"]
+        try:
+            lpa = entry["lpa"]
+            actor = entry["actor"]
+            dob = entry["dob"]
 
-        # 1. expire all existing codes for LPA/Actor combo
-        code_generator.update_codes(
-            database=db, key=key, status=False, status_details="Superseded"
-        )
+            key = {"lpa": lpa, "actor": actor}
 
-        # 2. generate a new code
-        generated_code = code_generator.generate_code(database=db)
+            # 1. expire all existing codes for LPA/Actor combo
+            code_generator.update_codes(
+                database=db, key=key, status=False, status_details="Superseded"
+            )
 
-        # 3. insert new code into database
-        new_code = code_generator.insert_new_code(
-            database=db, key=key, dob=dob, code=generated_code
-        )[0]["code"]
+            # 2. generate a new code
+            generated_code = code_generator.generate_code(database=db)
 
-        # 4. return the new code in lambda payload
-        response = {
-            "lpa": entry["lpa"],
-            "actor": entry["actor"],
-            "code": new_code,
-        }
+            # 3. insert new code into database
+            new_code = code_generator.insert_new_code(
+                database=db, key=key, dob=dob, code=generated_code
+            )[0]["code"]
 
-        code_list.append(response)
-        logger.info(f"code_list: {code_list}")
+            # 4. return the new code in lambda payload
+            response = {
+                "lpa": entry["lpa"],
+                "actor": entry["actor"],
+                "code": new_code,
+            }
+
+            code_list.append(response)
+            logger.info(f"code_list: {code_list}")
+        except KeyError:
+            return {"codes": None}
 
     return {"codes": code_list}
 
--- lambda_functions/v1/tests/api/test_create_handler.py
+++ lambda_functions/v1/tests/api/test_create_handler.py
@@ -23,7 +23,8 @@ def test_post(mock_database, mock_generate_code, case_data: CaseDataGetter):
 
     assert result == expected_result
 
-    remove_test_data(expected_result["codes"])
+    if expected_result["codes"] is not None:
+        remove_test_data(expected_result["codes"])
 
 
 @cases_data(module=cases_handle_create)
@@ -33,29 +34,44 @@ def test_data(mock_database, mock_generate_code, case_data: CaseDataGetter):
 
     insert_test_data(test_data=test_data)
 
-    handle_create(data=data)
+    codes_created = handle_create(data=data)
 
-    lpa = data["lpas"][0]["lpa"]
-    actor = data["lpas"][0]["actor"]
+    for i, item in enumerate(data):
 
-    db = boto3.resource("dynamodb")
-    table = db.Table(lpa_codes_table())
+        try:
+            lpa = data["lpas"][i]["lpa"]
 
-    query_result = table.query(
-        IndexName="key_index",
-        KeyConditionExpression=Key("lpa").eq(lpa) & Key("actor").eq(actor),
-    )
+            actor = data["lpas"][i]["actor"]
 
-    for item in query_result["Items"]:
+            dob = data["lpas"][i]["dob"]
 
-        if item["code"] == test_constants["DEFAULT_CODE"]:
-            assert item["active"] is True
-            assert item["status_details"] == "Generated"
-            assert item["last_updated_date"] == test_constants["TODAY_ISO"]
-            assert item["generated_date"] == test_constants["TODAY_ISO"]
-            assert item["expiry_date"] == test_constants["EXPIRY_DATE"]
-        else:
-            assert item["active"] is False
-            assert item["status_details"] == "Superseded"
+            db = boto3.resource("dynamodb")
+            table = db.Table(lpa_codes_table())
 
-    remove_test_data(expected_result["codes"] + test_data)
+            query_result = table.query(
+                IndexName="key_index",
+                KeyConditionExpression=Key("lpa").eq(lpa) & Key("actor").eq(actor),
+            )
+
+            print(f"query_result: {query_result}")
+
+            for item in query_result["Items"]:
+
+                if item["code"] == test_constants["DEFAULT_CODE"]:
+                    assert item["dob"] == dob
+                    assert item["active"] is True
+                    assert item["status_details"] == "Generated"
+                    assert item["last_updated_date"] == test_constants["TODAY_ISO"]
+                    assert item["generated_date"] == test_constants["TODAY_ISO"]
+                    assert item["expiry_date"] == test_constants["EXPIRY_DATE"]
+                else:
+                    assert item["active"] is False
+                    assert item["status_details"] == "Superseded"
+
+        except KeyError:
+            assert codes_created == {"codes": None}
+
+    if expected_result["codes"] is not None:
+        remove_test_data(expected_result["codes"] + test_data)
+    else:
+        remove_test_data(test_data)
