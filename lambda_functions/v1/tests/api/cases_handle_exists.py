ecde5fef88bef8ca30569dba4b7a84b8f936b007
GemTay <gemma.taylor@digital.justice.gov.uk>
2021-01-05 15:43:48 +0000

    added more test cases for exists handler

--- lambda_functions/v1/tests/api/cases_handle_exists.py
+++ lambda_functions/v1/tests/api/cases_handle_exists.py
@@ -67,3 +67,116 @@ def case_actor_code_exists_1() -> CaseData:
 
     expected_status_code = 200
     return test_data, data, expected_result, expected_status_code
+
+@case_name("Returns null if no active codes in query result")
+def case_actor_code_exists_2() -> CaseData:
+    test_data = [
+        {
+            "active": False,
+            "actor": "actor_1",
+            "code": "jmABs6fFaNJG",
+            "dob": "1960-06-05",
+            "expiry_date": test_constants["EXPIRY_DATE"],
+            "last_updated_date": "2019-12-26",
+            "lpa": "lpa_1",
+            "generated_date": "2019-09-31",
+            "status_details": "Revoked",
+        },
+        {
+            "active": False,
+            "actor": "actor_1",
+            "code": "MYX426fFaNJG",
+            "dob": "1960-06-05",
+            "expiry_date": test_constants["EXPIRY_DATE"],
+            "last_updated_date": "2019-12-26",
+            "lpa": "lpa_1",
+            "generated_date": "2019-09-31",
+            "status_details": "Revoked",
+        }
+    ]
+
+    data = {
+        "lpa": "lpa_1",
+        "actor": "actor_1"
+    }
+
+    expected_result = None
+
+    expected_status_code = 200
+    return test_data, data, expected_result, expected_status_code
+
+
+@case_name("Returns null with 200 if no match found")
+def case_actor_code_exists_3() -> CaseData:
+    test_data = [
+        {
+            "active": True,
+            "actor": "actor_2",
+            "code": "pt4F6fFaNJG",
+            "dob": "1966-12-09",
+            "expiry_date": test_constants["EXPIRY_DATE"],
+            "last_updated_date": "2019-12-26",
+            "lpa": "lpa_1",
+            "generated_date": "2019-09-31",
+            "status_details": "Superseded",
+        },
+        {
+            "active": False,
+            "actor": "actor_1",
+            "code": "MYX426fFaNJG",
+            "dob": "1960-06-05",
+            "expiry_date": test_constants["EXPIRY_DATE"],
+            "last_updated_date": "2019-12-26",
+            "lpa": "lpa_1",
+            "generated_date": "2019-09-31",
+            "status_details": "Revoked",
+        }
+    ]
+
+    data = {
+        "lpa": "lpa_1",
+        "actor": "actor_1"
+    }
+
+    expected_result = None
+
+    expected_status_code = 200
+    return test_data, data, expected_result, expected_status_code
+
+
+@case_name("Returns null with 200 if no match found")
+def case_actor_code_exists_3() -> CaseData:
+    test_data = [
+        {
+            "active": True,
+            "actor": "actor_2",
+            "code": "pt4F6fFaNJG",
+            "dob": "1966-12-09",
+            "expiry_date": test_constants["EXPIRY_DATE"],
+            "last_updated_date": "2019-12-26",
+            "lpa": "lpa_1",
+            "generated_date": "2019-09-31",
+            "status_details": "Superseded",
+        },
+        {
+            "active": False,
+            "actor": "actor_1",
+            "code": "MYX426fFaNJG",
+            "dob": "1960-06-05",
+            "expiry_date": test_constants["EXPIRY_DATE"],
+            "last_updated_date": "2019-12-26",
+            "lpa": "lpa_1",
+            "generated_date": "2019-09-31",
+            "status_details": "Revoked",
+        }
+    ]
+
+    data = {
+        "lpa": "lpa_1",
+        "actor": "actor_1"
+    }
+
+    expected_result = None
+
+    expected_status_code = 200
+    return test_data, data, expected_result, expected_status_code
