36a9d0c9c54326adbd2b186a49f4df29f9eeb3af
Tom Withers <thomas.withers@digital.justice.gov.uk>
2021-11-29 11:15:03 +0000

    SP-884 - Remove lambda that logs PII data

--- terraform/environment/modules/lambda/lambda.tf
+++ terraform/environment/modules/lambda/lambda.tf
@@ -40,45 +40,6 @@ resource "aws_lambda_function" "lambda_function" {
   tags = var.tags
 }
 
-resource "aws_lambda_permission" "lambda_permission" {
-  statement_id  = "AllowApiLPACodesGatewayInvoke-${var.environment}-${var.openapi_version}-${var.lambda_prefix}"
-  action        = "lambda:InvokeFunction"
-  function_name = aws_lambda_function.lambda_function.function_name
-  principal     = "apigateway.amazonaws.com"
-
-  source_arn = "${var.rest_api.execution_arn}/*/*/*"
-}
-
-resource "aws_lambda_function" "lambda_dbstream_function" {
-  filename         = data.archive_file.lambda_dynamodb_stream_archive.output_path
-  source_code_hash = data.archive_file.lambda_dynamodb_stream_archive.output_base64sha256
-  function_name    = "lpa-codes-dbstream-${var.environment}-${var.openapi_version}"
-  role             = aws_iam_role.lambda_role.arn
-  handler          = "dynamodb_stream.lambda_handler"
-  runtime          = "python3.7"
-  timeout          = 5
-  depends_on       = [aws_cloudwatch_log_group.lambda_dbstream]
-  vpc_config {
-    subnet_ids         = var.aws_subnet_ids
-    security_group_ids = [data.aws_security_group.lambda_api_ingress.id]
-  }
-  environment {
-    variables = {
-      API_VERSION = var.openapi_version
-    }
-  }
-  tracing_config {
-    mode = "Active"
-  }
-  tags = var.tags
-}
-
-resource "aws_lambda_event_source_mapping" "dynamodb_stream_map" {
-  event_source_arn  = var.dynamodb_table.stream_arn
-  function_name     = aws_lambda_function.lambda_dbstream_function.arn
-  starting_position = "LATEST"
-}
-
 resource "aws_lambda_layer_version" "lambda_layer" {
   filename         = data.archive_file.lambda_layer_archive.output_path
   source_code_hash = data.archive_file.lambda_layer_archive.output_base64sha256
