ba5104db901a24d6f75f18d857ba443ba5bbb931
Jim <58939809+jamesrwarren@users.noreply.github.com>
2023-10-10 09:51:40 +0100

    UML-3000 move to github actions (#132)

--- terraform/environment/modules/stage/variables.tf
+++ terraform/environment/modules/stage/variables.tf
@@ -1,34 +1,51 @@
-variable "account_name" {}
+variable "account_name" {
+  description = "Account name to use"
+  type        = string
+}
 
-variable "api_name" {}
+variable "api_name" {
+  description = "Name fo the API gateway"
+  type        = string
+}
 
 variable "aws_subnet_ids" {
-  type = list(string)
+  description = "List of subnets"
+  type        = list(string)
 }
 
-variable "domain_name" {}
-
-variable "environment" {
-  type = string
+variable "domain_name" {
+  description = "Domain name to use"
 }
 
-variable "lpa_codes_lambda" {}
+variable "lpa_codes_lambda" {
+  description = "Lambda Object"
+}
 
 variable "openapi_version" {
-  type    = string
-  default = "v1"
+  description = "Openapi version"
+  type        = string
+  default     = "v1"
 }
 
-variable "region_name" {}
+variable "image_tag" {
+  description = "Image tag for the lambda image"
+  type        = string
+}
 
-variable "rest_api" {}
+variable "region_name" {
+  description = "Region name"
+  type        = string
+}
 
-variable "tags" {}
+variable "rest_api" {
+  description = "The rest API Object"
+}
 
-variable "vpc_id" {
-  type = string
+variable "tags" {
+  description = "Tags to use"
 }
 
 variable "content_api_sha" {
-  type = string
+  description = "SHA for the content of the openapi spec"
+  type        = string
 }
