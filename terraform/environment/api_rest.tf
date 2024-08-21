88137faeb5af369342d4e85674dabb1601cd948c
gillettmoj <jason.gillett@digital.justice.gov.uk>
2024-08-21 10:32:47 +0100

    Typo on module reference

--- terraform/environment/api_rest.tf
+++ terraform/environment/api_rest.tf
@@ -9,7 +9,7 @@ resource "aws_api_gateway_rest_api" "lpa_codes" {
 
   lifecycle {
     replace_triggered_by = [
-      module.amdba_lpa_codes_v1.aws_iam_role.lambda_role
+      module.lamdba_lpa_codes_v1.aws_iam_role.lambda_role
     ]
   }
 
