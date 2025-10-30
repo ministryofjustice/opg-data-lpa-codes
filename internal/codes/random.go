77bb2fa6d77642f850689193390ae2e35c82f30f
Joshua Hawxwell <m@hawx.me>
2025-10-30 14:50:32 +0000

    Add endpoint to create paper verification codes

--- internal/codes/random.go
+++ internal/codes/random.go
@@ -1,17 +1,44 @@
 package codes
 
-import "crypto/rand"
-
-const (
-	codeSize    = 12
-	codeCharset = "346789BCDFGHJKMPQRTVWXY"
+import (
+	"crypto/rand"
+	"math/big"
 )
 
-func randomCode() string {
-	bytes := make([]byte, codeSize)
-	_, _ = rand.Read(bytes)
-	for i, b := range bytes {
-		bytes[i] = codeCharset[b%byte(len(codeCharset))]
+const (
+	activationCodeSize        = 12                        // unformatted
+	paperVerificationCodeSize = 19                        // formatted
+	codeCharset               = "346789BCDFGHJKMPQRTVWXY" // pragma: allowlist secret
+	codeCharsetLength         = int64(len(codeCharset))
+)
+
+func randomActivationCode() string {
+	out := make([]byte, activationCodeSize)
+	maxInt := big.NewInt(codeCharsetLength)
+
+	for i := range activationCodeSize {
+		n, _ := rand.Int(rand.Reader, maxInt)
+		out[i] = codeCharset[n.Int64()%codeCharsetLength]
 	}
-	return string(bytes)
+
+	return string(out)
+}
+
+func randomPaperVerificationCode() string {
+	out := make([]byte, paperVerificationCodeSize)
+	maxInt := big.NewInt(codeCharsetLength)
+
+	for i := range paperVerificationCodeSize {
+		switch i {
+		case 0:
+			out[i] = 'P'
+		case 1, 6, 11, 16:
+			out[i] = '-'
+		default:
+			n, _ := rand.Int(rand.Reader, maxInt)
+			out[i] = codeCharset[n.Int64()%codeCharsetLength]
+		}
+	}
+
+	return string(out)
 }
